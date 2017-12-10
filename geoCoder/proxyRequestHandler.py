2299ac2257ad214c440bc4733f7e9e2d474c916b
Sadanand Singh <sadanand4singh@gmail.com>
2017-12-09 16:42:49 -0800

    Add global status messages to proxyRequest Handler
    These messages can be used to provide user friendly messages when any
    error http status code is broadcast.

--- geoCoder/proxyRequestHandler.py
+++ geoCoder/proxyRequestHandler.py
@@ -6,12 +6,24 @@
 
 """
 
-class ProxyRequestHandler:
+import json
+from http.server import BaseHTTPRequestHandler
+from urllib.parse import urlparse, parse_qs
+from geoCoder import GeoCoder
+
+
+class ProxyRequestHandler(BaseHTTPRequestHandler):
     """HTTP Request handler component for the proxy server.
 
     It performs the do_GET operations for our proxy server. Uses helper
     classes :py:class:`GeoCoder <geoCoder>` to make requests to
     external service providers.
     """
+
+    messages = {}
+    messages[400] = 'Please provide an address to geocode your request query string (e.g. "?address=PostMates Office, CA")'
+    messages[503] = 'There are no geocoding services available. Try again later.'
+    messages[404] = 'Adrress did not found on any providers!'
+
 
     def do_GET(self):

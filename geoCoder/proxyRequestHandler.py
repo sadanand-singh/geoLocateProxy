dff8b2645d97c0965e6b4e13334014727f16351a
Sadanand Singh <sadanand4singh@gmail.com>
2017-12-09 16:46:29 -0800

    Add helper methods to recieve coords from providers
    Also send_json helper method to write output in json format

--- geoCoder/proxyRequestHandler.py
+++ geoCoder/proxyRequestHandler.py
@@ -50,3 +50,41 @@ class ProxyRequestHandler(BaseHTTPRequestHandler):
         self.__send_json(status, coords)
         return
 
+    def __geocoding(self):
+        """ Helper method to create geoCoder instance and call providers
+
+        Args:
+            None
+
+        Returns:
+            status, coords: Response status code and list of coords
+
+        """
+
+        geocoder = GeoCoder(self.primary)
+        uri = urlparse(self.path)
+        query = parse_qs(uri.query)
+        status = 400
+
+        if query.get('address') is None:
+            return status, None
+
+        status, coords = geocoder.get_geocode(query['address'][0])
+
+        return status, coords
+
+    def __send_json(self, http_status_code, data):
+        """Helper method to send json data to server
+
+        Args:
+            http_status_code: HTTP status code response from external API
+            data: relevant message for decoding the status, or coordinates
+
+        """
+
+        text = json.dumps(data)
+        self.send_response(http_status_code)
+        self.end_headers()
+        self.wfile.write(text.encode('utf-8'))
+
+        return

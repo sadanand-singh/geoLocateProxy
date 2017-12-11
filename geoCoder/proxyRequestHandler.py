f3cbed6da1bbba247cce6ca637c7f3faa97c56c3
Sadanand Singh <sadanand4singh@gmail.com>
2017-12-10 17:51:21 -0800

    Add LimitedSizeDict def for cache storage

--- geoCoder/proxyRequestHandler.py
+++ geoCoder/proxyRequestHandler.py
@@ -7,9 +7,30 @@
 """
 
 import json
+from collections import OrderedDict
+from functools import partial
 from http.server import BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
 from geoCoder import GeoCoder
+
+class LimitedSizeDict(OrderedDict):
+    """Dict with fixed size"""
+
+    def __init__(self, *args, **kwargs):
+        self.size_limit = kwargs.pop("size_limit", None)
+        OrderedDict.__init__(self, *args, **kwargs)
+        self._check_size_limit()
+
+    def __setitem__(self, key, value):
+        OrderedDict.__setitem__(self, key, value)
+        self._check_size_limit()
+
+    def _check_size_limit(self):
+        if self.size_limit is not None:
+            while len(self) > self.size_limit:
+                # Pop items in FIFO order
+                self.popitem(last=False)
+
 
 
 class ProxyRequestHandler(BaseHTTPRequestHandler):
